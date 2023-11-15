9b3aee5c22be774a2961c91c0533ced750c0fa87
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-11-15 12:51:51 +0100

    Use context to provide variables to all components

---
+++ app/View/Components/Layout.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Layout extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(public $context)
+    {
+        //
+    }
+
+    /**
+     * Build data
+     */
+    public function data(): array
+    {
+        return array_merge(parent::data(), $this->context);
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.layout');
+    }
+}
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,19 +1,21 @@
 <!doctype html>
 <html lang="{{ $locale }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-        <title>{{ $title }}</title>
-        @vite('resources/css/site.scss')
-    </head>
-    <body class="bg-gray-100 font-sans leading-normal text-gray-800">
-        <div class="mx-auto px-2 lg:min-h-screen flex flex-col items-center justify-center">
-            {{ $slot }}
-        </div>
-        @vite('resources/js/site.js')
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <title>{{ $title ?? "" }}</title>
+    <x-meta_tags></x-meta_tags>
+
+    @vite('resources/css/site.scss')
+</head>
+<body class="bg-gray-100 font-sans leading-normal text-gray-800">
+<div class="mx-auto px-2 lg:min-h-screen flex flex-col items-center justify-center">
+    {{ $slot }}
+</div>
+@vite('resources/js/site.js')
+</body>
 </html>
